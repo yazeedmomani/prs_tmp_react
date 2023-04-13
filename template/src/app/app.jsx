9d4a62bc5348297771e06c958581b2a783aa4a5c
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2023-04-13 08:42:56 +0300

    make some changes

--- template/src/app/app.jsx
+++ template/src/app/app.jsx
@@ -1,12 +1,5 @@
-import styles from "./app.module.scss";
+import TemplateOverview from "./template-overview";
 
 export default function App() {
-  return (
-    <div className={styles[`delete-me`]}>
-      <h1 className={styles[`delete-me__header`]}>Yazeedz React Template</h1>
-      <p className={styles[`delete-me__paragraph`]}>
-        Write some code to get started
-      </p>
-    </div>
-  );
+  return <TemplateOverview />;
 }
